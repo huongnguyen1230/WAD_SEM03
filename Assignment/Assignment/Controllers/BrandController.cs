5c6cf1cf21c52e80826cd6d0b6eb1788e485dbe5
nguyenhuong301201 <huongntth2002010@fpt.edu.vn>
2021-06-23 14:26:04 +0700

    huong

---
+++ Assignment/Assignment/Controllers/BrandController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: Brand
+        public ActionResult ViewBrand()
+        {
+            return View();
+        }
+    }
+}
