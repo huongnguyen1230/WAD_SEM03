a1f08ec716a543823a91476fe8f0accd8e90c365
nguyenhuong301201 <huongntth2002010@fpt.edu.vn>
2021-06-28 08:57:23 +0700

    update

---
+++ T2004E_WAD/T2004E_WAD/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using T2004E_WAD.Models;
+
+namespace T2004E_WAD.Context
+{
+    public class DataContext :DbContext
+    {
+        public DataContext() : base("WAD_T2004E")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
